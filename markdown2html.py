dad60a6a718ad9cdb3bfd42f6fb225f1bdb6a664
GoldenHatchet15 <raphsant20@gmail.com>
2024-08-06 14:56:44 -0400

    Tasks0

--- markdown2html.py
+++ markdown2html.py
@@ -2,7 +2,8 @@
 """
 Markdown to HTML converter script.
 
-This script takes two arguments: the input Markdown file and the output HTML file.
+This script takes two arguments: the input Markdown file
+and the output HTML file.
 It converts the Markdown content to HTML and writes it to the output file.
 """
 
@@ -10,14 +11,21 @@ import sys
 import os
 import re
 
+
 def markdown_to_html(markdown_text):
     # Converting headers
-    markdown_text = re.sub(r"(^|\n)###### (.*?)(\n|$)", r"\1<h6>\2</h6>\3", markdown_text)
-    markdown_text = re.sub(r"(^|\n)##### (.*?)(\n|$)", r"\1<h5>\2</h5>\3", markdown_text)
-    markdown_text = re.sub(r"(^|\n)#### (.*?)(\n|$)", r"\1<h4>\2</h4>\3", markdown_text)
-    markdown_text = re.sub(r"(^|\n)### (.*?)(\n|$)", r"\1<h3>\2</h3>\3", markdown_text)
-    markdown_text = re.sub(r"(^|\n)## (.*?)(\n|$)", r"\1<h2>\2</h2>\3", markdown_text)
-    markdown_text = re.sub(r"(^|\n)# (.*?)(\n|$)", r"\1<h1>\2</h1>\3", markdown_text)
+    markdown_text = re.sub(
+        r"(^|\n)###### (.*?)(\n|$)", r"\1<h6>\2</h6>\3", markdown_text)
+    markdown_text = re.sub(
+        r"(^|\n)##### (.*?)(\n|$)", r"\1<h5>\2</h5>\3", markdown_text)
+    markdown_text = re.sub(
+        r"(^|\n)#### (.*?)(\n|$)", r"\1<h4>\2</h4>\3", markdown_text)
+    markdown_text = re.sub(
+        r"(^|\n)### (.*?)(\n|$)", r"\1<h3>\2</h3>\3", markdown_text)
+    markdown_text = re.sub(
+        r"(^|\n)## (.*?)(\n|$)", r"\1<h2>\2</h2>\3", markdown_text)
+    markdown_text = re.sub(
+        r"(^|\n)# (.*?)(\n|$)", r"\1<h1>\2</h1>\3", markdown_text)
 
     # Converting bold text
     markdown_text = re.sub(r"\*\*(.*?)\*\*", r"<b>\1</b>", markdown_text)
@@ -30,14 +38,18 @@ def markdown_to_html(markdown_text):
         if not (p.startswith('<h') and p.endswith('>')):
             p = f"<p>{p}</p>"
         html_paragraphs.append(p)
-    
+
     html_text = "\n\n".join(html_paragraphs)
 
     return html_text
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print(
+            "Usage: ./markdown2html.py README.md README.html",
+            file=sys.stderr
+        )
         sys.exit(1)
 
     input_file = sys.argv[1]
@@ -50,10 +62,8 @@ if __name__ == "__main__":
     try:
         with open(input_file, 'r') as file:
             markdown_text = file.read()
-            print(f"Markdown content:\n{markdown_text}")  # Debugging print
             html_text = markdown_to_html(markdown_text)
-            print(f"HTML content:\n{html_text}")  # Debugging print
-        
+
         with open(output_file, 'w') as file:
             file.write(html_text)
     except Exception as e:
